87e4fb83189af85b190ef30b168f096c99ea1df5
Gautam <glodhiya89@gmail.com>
2013-06-21 01:24:47 +0530

    changed demo view

--- YAJLParser/YAJLViewController.m
+++ YAJLParser/YAJLViewController.m
@@ -25,7 +25,7 @@
 - (void)loadView {
     [super loadView];
     
-    UIButton *parseStreamJSONButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    parseStreamJSONButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     [parseStreamJSONButton setTitle:@"Parse Stream/Chunk JSON" forState:UIControlStateNormal];
     [parseStreamJSONButton setFrame:CGRectMake(20, 20, 50, 20)];
     [parseStreamJSONButton sizeToFit];
@@ -33,15 +33,28 @@
     [self.view addSubview:parseStreamJSONButton];
     
     
-    UIButton *parseCompleteJSONButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    parseCompleteJSONButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     [parseCompleteJSONButton setTitle:@"Parse Complete JSON" forState:UIControlStateNormal];
     [parseCompleteJSONButton setFrame:CGRectMake(20, parseStreamJSONButton.frame.origin.y + parseStreamJSONButton.frame.size.height + 20, 50, 20)];
     [parseCompleteJSONButton sizeToFit];
     [parseCompleteJSONButton addTarget:self action:@selector(parseCompleteEvent:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:parseCompleteJSONButton];
+    
+    
+    outputLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, CGRectGetMaxY(parseCompleteJSONButton.frame) + 20, CGRectGetWidth(self.view.frame) - 40, 20)];
+    outputLabel.font = [UIFont systemFontOfSize:14];
+    outputLabel.text = @"Human readable text";
+    [self.view addSubview:outputLabel];
+    
+    
+    outputTextView = [[UITextView alloc] initWithFrame:CGRectMake(20, CGRectGetMaxY(outputLabel.frame) + 5, CGRectGetWidth(self.view.frame) - 40, CGRectGetHeight(self.view.frame) - CGRectGetMaxY(outputLabel.frame) - 20)];
+    outputTextView.font = [UIFont systemFontOfSize:14];
+    outputTextView.backgroundColor = [UIColor lightGrayColor];
+    outputTextView.text = [NSString stringWithFormat:@"%@", @"SAMPLE OUTPUT"];
+    [self.view addSubview:outputTextView];
 }
 
-- (void)parseStreamEvent:(id)sender {    
+- (void)parseStreamEvent:(id)sender {
     [self parseStreamJSON];
 }
 
@@ -58,11 +71,20 @@
     // Handle response accordingly.
     NSLog(@"<key>: %@; <parents>: %@", key, parents);
     NSLog(@"<Dict>: %@: ", dictionary);
+    
+    
+    if ([key isEqualToString:@"glossary"]) {
+        NSMutableDictionary* titleDictionary = [dictionary objectForKey:@"title"];
+        NSString* title = (NSString *)[titleDictionary objectForNode];
+        outputTextView.text = [NSString stringWithFormat:@"Glossary title: %@", title];
+    }
 }
 
 
 
 - (void)parseStreamJSON {
+    NSLog(@"================================================\n");
+    NSLog(@"====== START parseStreamJSON");
     NSString* path = [[NSBundle mainBundle] pathForResource:@"stream1"
                                                      ofType:@"json"];
     NSString* chunk1 = [NSString stringWithContentsOfFile:path
@@ -76,59 +98,22 @@
                                                     error:NULL];
     
     
-    
     self.parser = [[JSONParser alloc] init];
     self.parser.JSONParserDelegate = self;
     self.parser.receiveEvents = YES;
-    self.status = [self.parser parse:chunk1];
-    self.status = [self.parser parse:chunk2];
+    [self.parser parse:chunk1];
+    [self.parser parse:chunk2];
     
-    
-    
-    if (!self.parser.receiveEvents) {
-        // Handle response accordingly.
-        
-        if (self.status == YAJLParserStatusInsufficientData || self.status == YAJLParserStatusOK) {
-            NSMutableDictionary* rootDictionary = (NSMutableDictionary*)[self.parser rootObject];
-            
-            
-            
-            
-            NSMutableDictionary* glossaryDictionary = [rootDictionary objectForKey:@"glossary"];
-            NSMutableDictionary* titleDictionary = [glossaryDictionary objectForKey:@"title"];
-            NSString* title = (NSString *)[titleDictionary objectForNode];
-            
-            
-            NSMutableDictionary* glossaryDivDictionary = [glossaryDictionary objectForKey:@"GlossDiv"];
-            NSMutableDictionary* dicTitleDictionary = [glossaryDivDictionary objectForKey:@"title"];
-            NSString* divTitle = (NSString *)[dicTitleDictionary objectForNode];
-            
-            
-            NSMutableDictionary* glossListDictionary = [glossaryDivDictionary objectForKey:@"GlossList"];
-            NSMutableDictionary* glossEntryDictionary = [glossListDictionary objectForKey:@"GlossEntry"];
-            NSMutableDictionary* glossDefDictionary = [glossEntryDictionary objectForKey:@"GlossDef"];
-            NSMutableDictionary* glossSeeAlsoDictionary = [glossDefDictionary objectForKey:@"GlossSeeAlso"];
-            
-            
-            
-            
-            NSLog(@"rootDictionary: %@", rootDictionary);
-            NSLog(@"title: %@", title);
-            NSLog(@"divTitle: %@", divTitle);
-            NSLog(@"glossSeeAlsoDictionary: %@", [glossSeeAlsoDictionary objectForNode]);
-        }
-        
-        if (self.status == YAJLParserStatusError) {
-            // error
-        }
-    }
-    
-    
-    NSLog(@"================================================");
+    NSLog(@"====== END parseStreamJSON");
+    NSLog(@"================================================\n");
     NSLog(@"\n\n\n\n\n\n\n");
 }
 
 - (void)parseCompleteJSON {
+    NSLog(@"================================================\n");
+    NSLog(@"====== START parseCompleteJSON");
+    
+    
     NSString* path = [[NSBundle mainBundle] pathForResource:@"complete" ofType:@"json"];
     NSString* chunk = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
     
@@ -139,35 +124,36 @@
     self.status = [self.parser parse:chunk];
     
     
-    if (!self.parser.receiveEvents) {
-        // Handle response accordingly.
+    if (self.status == YAJLParserStatusOK) {
+        NSMutableDictionary* rootDictionary = (NSMutableDictionary*)[self.parser rootObject];
         
-        if (self.status == YAJLParserStatusInsufficientData || self.status == YAJLParserStatusOK) {
-            NSMutableDictionary* rootDictionary = (NSMutableDictionary*)[self.parser rootObject];
-            
-            
-            NSMutableDictionary* menuDict = [rootDictionary objectForKey:@"menu"];
-            NSMutableDictionary* nameDict = [menuDict objectForKey:@"header"];
-            NSString* header = (NSString *)[nameDict objectForNode];
-            NSLog(@"header: %@", header);
-            
-            NSMutableDictionary* windowDict = [menuDict objectForKey:@"items"];
-            NSMutableArray* arr = (NSMutableArray*)[windowDict objectForNode];
-            for (NSMutableDictionary* dict in arr) {
-                if ([dict objectForKey:@"label"]) {
-                    NSMutableDictionary* idDict = [dict objectForKey:@"label"];
-                    NSLog(@"obj: %@", [idDict objectForNode]);
-                }
+        
+        NSMutableDictionary* menuDict = [rootDictionary objectForKey:@"menu"];
+        NSMutableDictionary* nameDict = [menuDict objectForKey:@"header"];
+        NSString* header = (NSString *)[nameDict objectForNode];
+        NSLog(@"header: %@", header);
+        
+        
+        outputTextView.text = [NSString stringWithFormat:@"Menu header: %@", header];
+        
+        
+        NSMutableDictionary* windowDict = [menuDict objectForKey:@"items"];
+        NSMutableArray* arr = (NSMutableArray*)[windowDict objectForNode];
+        for (NSMutableDictionary* dict in arr) {
+            if ([dict objectForKey:@"label"]) {
+                NSMutableDictionary* idDict = [dict objectForKey:@"label"];
+                NSLog(@"obj: %@", [idDict objectForNode]);
             }
-        }
-        
-        if (self.status == YAJLParserStatusError) {
-            // error
         }
     }
     
+    if (self.status == YAJLParserStatusError) {
+        // error
+    }
     
-    NSLog(@"================================================");
+    
+    NSLog(@"====== END parseCompleteJSON");
+    NSLog(@"================================================\n");
     NSLog(@"\n\n\n\n\n\n\n");
 }
 
